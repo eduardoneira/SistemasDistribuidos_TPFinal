95260f257cfca5bba6286d17fdddec5858c4cbaa
Eduardo Neira <edun327@gmail.com>
2017-07-18 00:30:16 -0300

    mqtt with ecipse-paho

--- Camara/setup/python.sh
+++ Camara/setup/python.sh
@@ -11,3 +11,7 @@ pip3 install pika --user
 
 echo "Instalando pygame para control de camara"
 pip3 install pygame --user
+
+echo "Instalando eclipse paho para cliente mqtt"
+pip install paho-mqtt
+cp -R /lib/python2.7/site-packages/paho /lib/python3.6/site-packages/
---
+++ CentroMonitoreoBarrial/tests/test_server_mqtt.py
@@ -0,0 +1,12 @@
+import paho.mqtt.client as mqtt
+
+broker_address="0.0.0.0" 
+#broker_address="iot.eclipse.org"
+print("creating new instance")
+client = mqtt.Client("test1",clean_session=False) #create new instance
+print("connecting to broker")
+client.connect(broker_address) #connect to broker
+print("Subscribing to topic","house/bulbs/bulb1")
+client.subscribe("house/bulbs/bulb1")
+print("Publishing message to topic","house/bulbs/bulb1")
+client.publish("house/bulbs/bulb1","OFF",qos=1,retain=True)
