f2e1ba60820c69edb9193a93ae7ccaea92e96c48
LuisAli22 <luisali@fi.uba.ar>
2017-07-27 02:45:10 -0300

    Agrego en el script los pasos para crear la base de datos. La base de datos ahora es unica. Para saber quien esta en rostros perdidos o en problemas legales, la persona tiene un campo que indica el estado

--- CentroMonitoreoCiudad/main.py
+++ CentroMonitoreoCiudad/main.py
@@ -1,15 +1,7 @@
 import psycopg2
-from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 def main():
     try:
-        #conn = psycopg2.connect("dbname='legalproblemsdatabase' user='postgres' host='localhost' password='postgres'")
-        con = psycopg2.connect("dbname='postgres' user='postgres' host = 'localhost' password='postgres'")
-        dbname = "legalproblemsdatabase"
-        con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-        cur = con.cursor()
-        cur.execute('CREATE DATABASE ' + dbname)
-        cur.close()
-        con.close()
+        connectionLegalProblems = psycopg2.connect("dbname='cmcdatabase' user='postgres' host='localhost' password='postgres'")
     except:
         print ("I am unable to connect to the database")
         #crear base de datos si no existen
