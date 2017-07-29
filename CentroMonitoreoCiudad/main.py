3e7002592063d6a2652bea6512d7ddbc7ecb8087
LuisAli22 <luisali@fi.uba.ar>
2017-07-29 18:19:57 -0300

    Problema corregido parcialmente: image_listener termina con SIGINT, http_query_handler termina con SIGTERM (terminate)

--- CentroMonitoreoCiudad/main.py
+++ CentroMonitoreoCiudad/main.py
@@ -1,7 +1,8 @@
 from multiprocessing import Process
 from ProcesadorDeImagenes.image_listener import image_listener_start
 from ProcesadorDeImagenes.HTTP_query_handler import HTTP_query_handler_start
-
+import os
+import signal
 def main():
     #crear predictor
     http_query_handler_process = Process(target=HTTP_query_handler_start)
@@ -12,7 +13,8 @@ def main():
     while not user_input == 'q':
         user_input = input("Ingrese q para terminar:")
     http_query_handler_process.terminate()
-    image_listener_process.terminate()
+    #image_listener_process.terminate()
+    os.kill(image_listener_process.pid, signal.SIGINT)
     http_query_handler_process.join()
     image_listener_process.join()
 if __name__ == '__main__':
