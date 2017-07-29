08f5e2095102ed9953b731157ba36e6bb042c95c
LuisAli22 <luisali@fi.uba.ar>
2017-07-29 00:54:36 -0300

    Corrijo la forma en que se llamaba a los comandos de base de datos

--- CentroMonitoreoCiudad/ProcesadorDeImagenes/HTTP_query_handler.py
+++ CentroMonitoreoCiudad/ProcesadorDeImagenes/HTTP_query_handler.py
@@ -11,27 +11,36 @@ with open('config.json') as config_file:
   config = json.load(config_file)
 
 def handle(request,database,face_recognizer):
-  request = json.load(request)
-  response = {}
-  response['status'] = 'OK'
-
-  if (request.type == config['requests']['existance']):
-    id = face_recognizer.predict_base64(request.image)
-    respone['found'] = (id is not None)
-  elif (request.type == config['requests']['upload']):
-    face_recognizer.predict_base64(request.image)
-    response['id'] = str(id)
-    database.execute('INSERT INTO person (hashperson,state) VALUES (%s,%s)',(str(id),request['state']))
-  elif (request.type == config['requests']['trajectory']):
-    id = face_recognizer.predict_base64(request.image)
-    if id is not None:
-      database.execute('SELECT DISTINCT B.lat, B.lng FROM cmcdatabase.cropface C, cmcdatabase.bigpic B WHERE C.hashBigPic = B.hashBigPic AND C.HashPerson == (%s)',str(id))
-      response['coordinates'] = database.fetchone()
-  else:
-    response['status'] = 'ERROR'
-    response['message'] = 'Tipo de mensaje invalido'
-
-  return json.dumps(response)
+    request = json.load(request)
+    response = {}
+    response['status'] = 'OK'
+    with open('../Database/config.json') as f:
+        conf = json.load(f)
+        connection_str = "dbname={} user={} host={} password={}".format(conf['dbname'], conf['user'], conf['host'], conf['password'])
+        connection = psycopg2.connect(connection_str)
+        cursor = connection.cursor()
+        if (request.type == config['requests']['existance']):
+            id = face_recognizer.predict_base64(request.image)
+            response['found'] = (id is not None)
+        elif (request.type == config['requests']['upload']):
+            face_recognizer.predict_base64(request.image)
+            response['id'] = str(id)
+            #database.execute('INSERT INTO person (hashperson,state) VALUES (%s,%s)',(str(id),request['state']))
+            cursor.execute("""INSERT INTO Person (HashPerson,state) VALUES (%s,%s)""", id,request['state'])
+            connection.commit()
+        elif (request.type == config['requests']['trajectory']):
+            id = face_recognizer.predict_base64(request.image)
+        if id is not None:
+            #database.execute('SELECT DISTINCT B.lat, B.lng FROM cmcdatabase.cropface C, cmcdatabase.bigpic B WHERE C.hashBigPic = B.hashBigPic AND C.HashPerson == (%s)',str(id))
+            cursor.execute("SELECT DISTINCT B.lat, B.lng FROM cmcdatabase.cropface C, cmcdatabase.bigpic B WHERE C.hashBigPic = B.hashBigPic AND C.HashPerson == (%s)", id)
+            response['coordinates'] = cursor.fetchone()
+            connection.commit()
+        else:
+            response['status'] = 'ERROR'
+            response['message'] = 'Tipo de mensaje invalido'
+        cursor.close()
+        connection.close()
+    return json.dumps(response)
 
 def HTTP_query_handler_run(face_recognizer,database):
   print('Configurando CMC Query Handler')
--- CentroMonitoreoCiudad/ProcesadorDeImagenes/image_listener.py
+++ CentroMonitoreoCiudad/ProcesadorDeImagenes/image_listener.py
@@ -20,19 +20,21 @@ def callback(ch, method, properties, body):
         cursor = connection.cursor()
         payload = json.loads(body)
         logging.debug('Mensaje recibido: %d caras, con %s, %s',len(payload['faces']),payload['location'],payload['timestamp'])
-        sha1= Sha1()
+        #sha1= Sha1()
         #hash_big_pic = sha1.compute(payload['frame'])
         #location = payload['location']
         #latitude = location[0]
         #longitude = location[1]
         #timestamp = location['timestamp']
         #cursor.execute("""INSERT INTO BigPic (HashBigPic, Lat, Lng, Timestmp) VALUES (%s, %s, %s, %s)""",(hash_big_pic, latitude, longitude, timestamp))
-        for crop_face in payload['faces']:
-            #if feature_matcher.compare_to_all_faces(crop_face) == ONEMATCH:
-            #     img = feature_matcher.getMatch()
-            #     hash_person = sha1.compute(img)
-            #     hash_crop = sha1.compute(crop_face)
-            #     cursor.execute("""INSERT INTO CropFace (HashCrop, HashPerson, HashBigPic) VALUES (%s, %s, %s, %s);""",(hash_crop,hash_person, hash_big_pic))
+        #cursor.commit()
+        #for crop_face in payload['faces']:
+        #   if feature_matcher.compare_to_all_faces(crop_face) == ONEMATCH:
+        #   img = feature_matcher.getMatch()
+        #   hash_person = sha1.compute(img)
+        #   hash_crop = sha1.compute(crop_face)
+        #   cursor.execute("""INSERT INTO CropFace (HashCrop, HashPerson, HashBigPic) VALUES (%s, %s, %s, %s);""",(hash_crop,hash_person, hash_big_pic))
+        #   cursor.commit()
         cursor.close()
         connection.close()
 
