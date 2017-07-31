84aa292ab173e9442da4122d42b9170e213062a7
LuisAli22 <luisali@fi.uba.ar>
2017-07-30 21:52:45 -0300

    Peuqño cambio cuando recibe imagenes image_listener

--- CentroMonitoreoCiudad/ProcesadorDeImagenes/image_listener.py
+++ CentroMonitoreoCiudad/ProcesadorDeImagenes/image_listener.py
@@ -10,29 +10,41 @@ from ProcesadorDeImagenes.modules.Matcher import Matcher
 #from ProcesadorDeImagenes.modules.pika_wrapper_publisher import *
 from ProcesadorDeImagenes.modules.LBPH_wrapper import *
 sys.path.insert(0, '../')
+from CentroMonitoreoBarrial.modules.face_cropper import FaceCropper
 from Utils.Hash import compute_sha1_from_file
 from Utils.File_Manager import save_data_to_file
 import Utils.const as CONST
-def store_big_pic(payload, cursor, connect):
-    hash_big_pic, file_already_exists, filepath = save_data_to_file(payload['frame'], CONST.BIGPICIMAGECONTAINERFOLDER)
+def store_big_pic(payload, cursor, connect, cropper):
+    img = cropper.bytes_to_image(payload['frame'])
+    hash_big_pic, file_already_exists, filepath = save_data_to_file(img, CONST.BIGPICIMAGECONTAINERFOLDER)
     if not file_already_exists:
+        cursor.execute("SELECT * FROM BigPic WHERE  BigPic.HashBigPic = %s", (self.hash_big_pic,))
+        rows = cursor.fetchall()
+        if len(rows) == 0:
             location = payload['location']
             latitude = location[0]
             longitude = location[1]
             timestamp = payload['timestamp']
             cursor.execute("""INSERT INTO BigPic (HashBigPic, Lat, Lng, Timestmp) VALUES (%s, %s, %s, %s)""",(hash_big_pic, latitude, longitude, timestamp))
             connect.commit()
-def store_crop_faces(payload, cursor, connect):
+        else:
+            cursor.execute("UPDATE BigPic SET  BigPic (Lat, Lng, Timestmp) VALUES (%s, %s, %s)", (latitude, longitude, timestamp))
+def store_crop_faces(payload, cursor, connect, cropper):
     for crop_face in payload['faces']:
-        hash_crop, file_already_exists, filepath = save_data_to_file(crop_face, CONST.CROPIMAGECONTAINERFOLDER)
+        img = cropper.bytes_to_image(crop_face)
+        hash_crop, file_already_exists, filepath = save_data_to_file(img, CONST.CROPIMAGECONTAINERFOLDER)
         if not file_already_exists:
-            matcher = Matcher()
-            prediction_filename = matcher.predict(filepath)
-            if not prediction_filename == None:
-               hash_person = compute_sha1_from_file(prediction_filename)
-               cursor.execute("""INSERT INTO CropFace (HashCrop, HashPerson, HashBigPic) VALUES (%s, %s, %s, %s);""",(hash_crop,hash_person, hash_big_pic))
-               connect.commit()
+            cursor.execute("SELECT * FROM CropFace WHERE  CropFace.HashCrop = %s", (self.hash_crop,))
+            rows = cursor.fetchall()
+            if len(rows) == 0:
+                matcher = Matcher()
+                prediction_filename = matcher.predict(filepath)
+                if not prediction_filename == None:
+                   hash_person = compute_sha1_from_file(prediction_filename)
+                   cursor.execute("""INSERT INTO CropFace (HashCrop, HashPerson, HashBigPic) VALUES (%s, %s, %s, %s);""",(hash_crop,hash_person, hash_big_pic))
+                   connect.commit()
 def callback(ch, method, properties, body):
+    cropper = FaceCropper()
     payload = json.loads(body.decode('utf-8'))
     logging.debug('Mensaje recibido: {%s,%s}', payload['location'],payload['timestamp'])
     print("Se recibio mensaje de frame. Guardo la imagen grupal y las caras individuales")
@@ -42,8 +54,8 @@ def callback(ch, method, properties, body):
         connection_str = "dbname={} user={} host={} password={}".format(conf['dbname'], conf['user'], conf['host'], conf['password'])
         connection = psycopg2.connect(connection_str)
         cursor = connection.cursor()
-        store_big_pic(payload, cursor, connection)
-        store_crop_faces(payload, cursor, connection)
+        store_big_pic(payload, cursor, connection, cropper)
+        store_crop_faces(payload, cursor, connection, cropper)
         cursor.close()
         connection.close()
 
@@ -59,7 +71,6 @@ def image_listener_start():
 
   server = PikaWrapperSubscriber( host=config['host_CMB'],
                                   topic=config['topic_cmc'])
-
   # client = PikaWrapperPublisher(host=config['host_HTTP'],
   #                               topic=config['topic_'])
 
