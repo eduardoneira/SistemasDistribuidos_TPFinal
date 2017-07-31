e174ef3e7bbd4a7fbb52642d38919b1883382b1c
Eduardo Neira <edun327@gmail.com>
2017-07-31 02:12:50 -0300

    face recognizer desacoplado en proceso propio, http listener tambien pero falta guardar en file system

---
+++ CentroMonitoreoCiudad/HTTP_listener/HTTP_query_handler.py
@@ -0,0 +1,71 @@
+#!/bin/python3
+
+import json
+import psycopg2
+
+from modules.logger import *
+from modules.rpc_server_rabbitmq import *
+from modules.face_recognizer_client import *
+from modules.graceful_killer import *
+
+with open('./config.json') as config_file:
+  config = json.load(config_file)
+
+with open('../Database/config.json') as file:
+  conf_database = json.load(file)
+
+#TODO: save in FS
+def handle(request):
+  request = json.load(request)
+  response = {}
+  response['status'] = 'OK'
+    
+  if (request.type == config['requests']['existance']):
+    id = face_recognizer_client.predict([request['image']])[0]
+    response['found'] = (id is not None)
+  elif (request.type == config['requests']['upload']):
+    id = face_recognizer_client.update(request['image'])
+    response['id'] = str(id)
+    #database.execute('INSERT INTO person (hashperson,state) VALUES (%s,%s)',(str(id),request['state']))
+    cursor.execute("""INSERT INTO Person (HashPerson,state) VALUES (%s,%s)""",id,request['state'])
+  elif (request.type == config['requests']['trajectory']):
+    id = face_recognizer_client.predict([request['image']])[0]
+    if id is not None:
+      #database.execute('SELECT DISTINCT B.lat, B.lng FROM cmcdatabase.cropface C, cmcdatabase.bigpic B WHERE C.hashBigPic = B.hashBigPic AND C.HashPerson == (%s)',str(id))
+      cursor.execute("SELECT DISTINCT B.lat, B.lng FROM cmcdatabase.cropface C, cmcdatabase.bigpic B WHERE C.hashBigPic = B.hashBigPic AND C.HashPerson == (%s)", id)
+      response['coordinates'] = cursor.fetchone()
+  else:
+    response['status'] = 'ERROR'
+    response['message'] = 'Tipo de mensaje invalido'
+
+  return json.dumps(response)
+
+
+if __name__ == '__main__':
+  print('Configurando CMC Query Handler')
+
+  set_logger(config['logging_level'])
+
+  connection_str = "dbname={} user={} host={} password={}".format(conf_database['dbname'], conf_database['user'], conf_database['host'], conf_database['password'])
+  connection_db = psycopg2.connect(connection_str)
+  connection_db.autocommit = True
+
+  cursor = connection_db.cursor()
+
+  server = RPCServer( host='localhost',
+                      queue=config['queue_http'],
+                      request_callback_main=handle)
+
+  face_recognizer_client = FaceRecognizerClient(host='localhost',
+                                                queue_send=config['queue_send_face_recognizer'],
+                                                queue_receive=config['queue_receive_face_recognizer'])
+
+  graceful_killer = GracefulKiller()
+  graceful_killer.add_connection(server)
+  graceful_killer.add_connection(face_recognizer_client)
+  graceful_killer.add_connection(cursor)
+  graceful_killer.add_connection(connection_db)
+
+  print('Comenzando a escuchar mensajes rpc')
+  server.start()
+ 
--- CentroMonitoreoCiudad/ReconocedorDeCaras/modules/pika_wrapper_receive.py
+++ CentroMonitoreoCiudad/ReconocedorDeCaras/modules/pika_wrapper_receive.py
@@ -18,10 +18,19 @@ class PikaWrapperReceiver:
 
     logging.debug('Se establecio una conexion para recibir mensajes con el host: '+ host+' y escuchando de la cola: '+ queue)
 
+  def template_callback(self,ch,method, props, body):
+    response = self.callback()
+    ch.basic_publish(exchange='',
+                     routing_key=props.reply_to,
+                     body=str(response))
+
+    ch.basic_ack(delivery_tag = method.delivery_tag)
+
   def set_receive_callback(self,callback):
-    self.tag = self.channel.basic_consume(callback,
-                                          queue=self.queue,
-                                          no_ack=True)
+    self.callback = callback
+
+    self.tag = self.channel.basic_consume(self.template_callback,
+                                          queue=self.queue)
     return self.tag
 
   def start_consuming(self):
