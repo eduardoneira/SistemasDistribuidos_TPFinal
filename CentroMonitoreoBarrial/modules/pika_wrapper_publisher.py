71fff1b570edb88b17965040c51fc00b703442ab
Eduardo Neira <eneira@keepcon.com>
2017-08-02 13:51:17 -0300

    reconnect para CMB, hay checkear como hacerlo bien en el cmc y web

--- CentroMonitoreoBarrial/modules/pika_wrapper_publisher.py
+++ CentroMonitoreoBarrial/modules/pika_wrapper_publisher.py
@@ -6,22 +6,33 @@ import logging
 class PikaWrapperPublisher:
 
   def __init__(self,host,topic):
-    self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
-    
-    self.channel = self.connection.channel()
-
-    self.channel.exchange_declare(exchange=topic,
-                                  type='topic')
-
     self.host = host
     self.topic = topic
 
-    logging.debug('Se establecio una conexion como publisher con el host: '+ host+' con topic: '+topic)
+    self.__connect()
+
+    logging.debug('Se establecio una conexion como publisher con el host: '+host+' con topic: '+topic)
+
+  def __connect(self):
+    self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.host))
+    
+    self.channel = self.connection.channel()
+
+    self.channel.exchange_declare(exchange=self.topic,
+                                  type='topic')
 
   def send(self,message):
+    try: 
+      self.__publish(message)
+    except pika.exceptions.ConnectionClosed:
+      logging.warning('Se perdio la conexion, volviendo a conectarse')
+      self.__connect()
+      self.__publish(message)
+
+  def __publish(self,message):
     self.channel.basic_publish(exchange=self.topic,
-                               routing_key='',
-                               body=message)
+                           routing_key='',
+                           body=message)
 
   def sleep(self,time):
     self.connection.sleep(time)
