1e3503b9e0dc3086cb8498ffc55920b35e3facae
Eduardo Neira <edun327@gmail.com>
2017-08-15 21:21:17 -0300

    #14 forgot the module

---
+++ Camara/modules/rasp_camera.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import picamera
+import abstract_camera
+
+class RaspCamera(AbstractCamera):
+
+  CAPTURE_FILENAME = 'rasp_image.jpg'
+
+  def  __init__(self):
+    super().__init__()
+    self.camera = picamera.PiCamera()
+    #TODO: config camera
+
+  def get_frame(self):
+    filepath = self.PATH_IMG + self.CAPTURE_FILENAME
+    self.camera.capture(filepath)
+
+    with open(filepath,'rb') as image:
+      img_b64 = self.base64(image)
+
+  os.rm(filepath)      
+
+    return img_b64
